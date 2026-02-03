297c04e590009694327ff34a3ae6e4c4b799fca5
MadeyeMaddy <maddyhenney@gmail.com>
2026-02-03 20:44:38 +0000

    Start of implementing a settings menu

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -5,3 +5,7 @@ func _on_start_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+#settings scene called from main menu via button
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menus/settings_menu.tscn")
---
+++ scripts/menus/settings_menu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+#Allow the user to change audio volume.
+ 
+#Maybe include options for fullscreen or not. 
