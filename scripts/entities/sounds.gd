aca4c47f76e7b7d748234ee8c437b8789065d4c4
MadeyeMaddy <maddyhenney@gmail.com>
2026-02-06 23:51:16 +0000

    sound list

---
+++ scripts/entities/sounds.gd
@@ -0,0 +1,15 @@
+class_name sounds
+extends Node
+
+#const COMMON: String = "Common"
+const badfish_bleghspit: String = "res://resources/audio/sfx/badfish_bleghspit.wav"
+const badfish_eurgghhdisgusting: String = "res://resources/audio/sfx/badfish_eurgghhdisgusting.wav"
+const badfish_slurperherugh: String = "res://resources/audio/sfx/badfish_slurperherugh.wav"
+const badfish_slurplipsmackeughh: String = "res://resources/audio/sfx/badfish_slurplipsmackeughh.wav"
+const goodfish_ahhh: String = "res://resources/audio/sfx/goodfish_ahhh.wav"
+const goodfish_delicious: String = "res://resources/audio/sfx/goodfish_delicious.wav"
+const goodfish_mmm: String = "res://resources/audio/sfx/goodfish_mmm.wav"
+const goodfish_lipsmackingyeah: String = "res://resources/audio/sfx/goodfish_lipsmackingyeah.wav"
+const goodfish_nice: String = "res://resources/audio/sfx/goodfish_nice.wav"
+const goodfish_yummy: String = "res://resources/audio/sfx/goodfish_yummy.wav"
+const goodfish_slurplipsmackmmm: String = "res://resources/audio/sfx/goodfish_slurplipsmackmmm.wav"
