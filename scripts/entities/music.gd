2163dc6357ac725cbc47d6752ac4963d75aab71a
MadeyeMaddy <maddyhenney@gmail.com>
2026-02-07 12:22:50 +0000

    GJAM-35 created music track list
    Made a track list for music

---
+++ scripts/entities/music.gd
@@ -0,0 +1,7 @@
+class_name music
+extends Node
+
+# list all all music tracks for the game
+const sleepy_sailor_1: String = "res://resources/audio/music/sleepy_sailor_1_trimmed.wav"
+const sleepy_sailor_2: String = "res://resources/audio/music/sleepy_sailor_2_trimmed.wav"
+const sleepy_sailor_3: String = "res://resources/audio/music/sleepy_sailor_3_trimmed.wav"
