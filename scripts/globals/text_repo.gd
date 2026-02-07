a25e1681f0fc50be199d0e4af26c8af24af6e6ae
MadeyeMaddy <maddyhenney@gmail.com>
2026-02-07 23:08:59 +0000

    Update text_repo.gd
    Intro text

--- scripts/globals/text_repo.gd
+++ scripts/globals/text_repo.gd
@@ -1,5 +1,8 @@
 extends Node
 
+#Intro text
+const intro_text_to_game: String = "You wake up, again. Still here in this dreary place. /n Another day at work, surely soon you’ll have enough money to get far from here. /n Far from the horrid sea. /n Anyway, it’s time for work, better get up and out. /n The fish won’t sort themselves."
+
 # Reference book text
 const reference_book_page_zero_title: String = "Welcome!"
 const reference_book_page_zero_text: String = "'We were all fish once' - Old Gregg \nIf you can't tell a Cheese from a Fog Nose, you don't belong in the waters. This book contains everything you need to know about fish. More importantly, this book describes how to identify the 'good' fish from the 'bad' fish. If something doesn't look right with you catch, dispose of it as soon as possible. If you're reading this AFTER eating a bad fish, run as far away from civilization as you can..... Enjoy!"
