fbe499fdf5f578cbdfb9ebe9fee26f2fde058149
Seb <sebmarsontk@gmail.com>
2026-02-07 23:30:47 +0000

    GJAM-98 - Implement lick tool

--- scripts/globals/sfx.gd
+++ scripts/globals/sfx.gd
@@ -1,6 +1,17 @@
 class_name SFX
 extends Node
 
+const badfish_bleghspit = "res://resources/audio/sfx/fish_noises/badfish_bleghspit.wav"
+const badfish_eurgghhdisgusting = "res://resources/audio/sfx/fish_noises/badfish_eurgghhdisgusting.wav"
+const badfish_slurperugherugh = "res://resources/audio/sfx/fish_noises/badfish_slurperugherugh.wav"
+const badfish_slurplipsmackeughh = "res://resources/audio/sfx/fish_noises/badfish_slurplipsmackeughh.wav"
+const bouy_bell = "res://resources/audio/sfx/464856__petebarry__fx-ship-bell.wav"
+const goodfish_ahhh = "res://resources/audio/sfx/fish_noises/goodfish_ahhh.wav"
+const goodfish_delicious = "res://resources/audio/sfx/fish_noises/goodfish_delicious.wav"
+const goodfish_lipsmackingyeah = "res://resources/audio/sfx/fish_noises/goodfish_lipsmackingyeah.wav"
+const goodfish_mmm = "res://resources/audio/sfx/fish_noises/goodfish_mmm.wav"
+const goodfish_nice = "res://resources/audio/sfx/fish_noises/goodfish_nice.wav"
+const goodfish_slurplipsmackmmm = "res://resources/audio/sfx/fish_noises/goodfish_slurplipsmackmmm.wav"
+const goodfish_yummy = "res://resources/audio/sfx/fish_noises/goodfish_yummy.wav"
 const ocean_ambiance = "res://resources/audio/sfx/207553__bone666138__ocean-ambience-costal-with-foghorn.wav"
 const sea_waves = "res://resources/audio/sfx/2089_sea-waves-01.wav"
-const bouy_bell = "res://resources/audio/sfx/464856__petebarry__fx-ship-bell.wav"
