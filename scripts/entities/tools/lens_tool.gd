6a2258806d443d2801b456f8270aa2819e8ef1ee
Seb <sebmarsontk@gmail.com>
2026-02-07 23:12:08 +0000

    GJAM-88 - Lens tool implemented

---
+++ scripts/entities/tools/lens_tool.gd
@@ -0,0 +1,7 @@
+extends Tool
+
+func _ready() -> void:
+	custom_minimum_size = ($ToolTexture.size * $ToolTexture.scale)
+
+func get_tool_result(fish: Fish) -> String:
+	return "DOES THE FISH GLOW?... " + str(fish.glow)
