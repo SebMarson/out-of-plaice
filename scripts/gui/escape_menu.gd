85ccf4e33a606f314420ac7281f3e3f682c68cbd
MadeyeMaddy <maddyhenney@gmail.com>
2026-02-08 21:51:35 +0000

    Removed settings from escape menu

--- scripts/gui/escape_menu.gd
+++ scripts/gui/escape_menu.gd
@@ -29,9 +29,6 @@ func _on_main_menu_button_pressed() -> void:
 	get_tree().set_pause(false)
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
 
-func _on_settings_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/menus/settings_menu.tscn")
-
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit(0)
