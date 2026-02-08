782e40a73d765da3e37c31617c1accbc6e1f0843
MadeyeMaddy <maddyhenney@gmail.com>
2026-02-08 21:31:36 +0000

    bug fixes

--- scripts/gui/escape_menu.gd
+++ scripts/gui/escape_menu.gd
@@ -30,7 +30,8 @@ func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
 
 func _on_settings_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/menus/settings_menu.tscn")
+
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit(0)
