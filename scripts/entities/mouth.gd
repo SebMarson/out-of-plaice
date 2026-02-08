a99670ea5cda815edb67339e16a8c982f8d0dd98
MadeyeMaddy <maddyhenney@gmail.com>
2026-02-08 00:06:11 +0000

    yes

--- scripts/entities/mouth.gd
+++ scripts/entities/mouth.gd
@@ -1,6 +1,7 @@
 class_name Mouth
 extends Node
 
+<<<<<<< Updated upstream
 const PARASITE: String = "A parasite"
 const NO_PARASITE: String = "No parasite"
 
@@ -8,3 +9,8 @@ const NO_PARASITE: String = "No parasite"
 static func get_random() -> String:
 	var all_mouth = [PARASITE, NO_PARASITE]
 	return all_mouth[randi_range(0, all_mouth.size()-1)]
+=======
+
+const PARASITE: String = "A parasite!"
+const NO_PARASITE: String = "No parasite"
+>>>>>>> Stashed changes
