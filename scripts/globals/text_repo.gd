38b825d02906d0eedd770255fc7a10b851f10349
Jttg <jonasttg@gmail.com>
2026-02-08 16:53:39 +0000

    Updated text repo and trimmed page turn audio

--- scripts/globals/text_repo.gd
+++ scripts/globals/text_repo.gd
@@ -1,7 +1,30 @@
 extends Node
 
 #Intro text
-const intro_text_to_game: String = "You wake up, again. Still here in this dreary place. /n Another day at work, surely soon you’ll have enough money to get far from here. /n Far from the horrid sea. /n Anyway, it’s time for work, better get up and out. /n The fish won’t sort themselves."
+const intro_text_to_game: String = "You wake up, again. Still here in this dreary place. \n Another day at work, surely soon you’ll have enough money to get far from here. \n Far from the horrid sea. \n Anyway, it’s time for work, better get up and out. \n The fish won’t sort themselves."
+
+#Game Over text - Player causes too much corruption
+const gameover_text_corruption: String = "You have allowed too many bad fish to be eaten.
+\nThe local community has become too corrupted.
+\nYou no longer have any business. 
+\nJust the sound of the ocean.
+\nAnd the horrors within."
+
+#Game Over text - Player doesn't make enough money
+const gameover_text_bad_ending: String = "The day has finally come to pack your bags and get as far away as you possibly can from this accursed place. 
+\nYou count the money you've earned from sorting those fish-adjacent nightmares all week.
+\nThe dread catches up with you. 
+\nYou don't have enough money to survive out there. You'll run out of fuel and starve well before you make it to any kind of civilisation.
+\nYour hand trembles. The few coins you have fall, onto the floor, through the cracks and into the water below.
+\nThe only thing left for you to do:"
+
+#Game Over text - Player DOES make enough money
+const gameover_text_good_ending: String = "The day has finally come to pack your bags and get as far away as you possibly can from this accursed place. 
+\nYou count the money you've earned from sorting those fish-adjacent nightmares all week.
+\nMore than enough!
+\nYou take a deep breathe of the ocean spray, full of salt and sinister thoughts, and head straight for your vehicle.
+\nYou pray to whatever will listen that you'll never have to see the ocean or another fish again. 
+\nYou don't hesitate to get Out of this Plaice."
 
 # Reference book text
 const reference_book_page_zero_title: String = "Welcome!"
@@ -57,5 +80,27 @@ const reference_book_page_five_value: String = "Medium"
 const reference_book_page_five_rarity: String = "Common"
 const reference_book_page_five_glow: String = "Should not glow"
 
+<<<<<<< Updated upstream
 # Failure text coruption
 const failure_text_coruption: String = "You have allowed too many 'bad' fish to be eaten. /n The local community has become too corrupted. /n You no longer have any business. /n Just the sound of the ocean. /n And the horrors within. /n "
+=======
+#Pages for tools
+const reference_book_page_six_title: String = "Tools"
+const reference_book_page_six_text: String = "Knowing your good fish from your bad fish is as easy as knowing what tools to use:"
+
+const reference_book_page_seven_title: String = "Scales"
+const reference_book_page_seven_text: String = "No not fish scales. Scales for weighing. \nIf the weight of the fish doesn't match what's in the book. \nSomething's not right. Get rid of it!"
+
+const reference_book_page_eight_title: String = "Check the mouth"
+const reference_book_page_eight_text: String = "Alright this is less of a tool and more of an inspection. \nLook in the fish's mouth. Sometimes they have a parasite in there which most fish shouldn't have. \nSome fish are actually safer with one. Check the book to be sure."
+
+const reference_book_page_nine_title: String = "Lens"
+const reference_book_page_nine_text: String = "This might sound crazy but if you look at a fish using this lens, sometimes they glow! \nI dunno if it's radiation or something more sinister but whatever it is, make sure the fish is SUPPOSED to glow before you go serving it for dinner."
+
+const reference_book_page_ten_title: String = "Aura"
+const reference_book_page_ten_text: String = "Your gut is a tool. Trust it. \nTake a moment to read the aura of the fish. \nEvery fish has an aura and it's up to you to figure out if it's the right aura. \nIf the aura is off, chances are everything about the fish is off as well."
+
+#Lick has been removed
+#const reference_book_page_eleven_title: String = "Lick"
+#const reference_book_page_elven_text: String = "Absolutely useless. Don't bother. Unless you like the taste of raw fish."
+>>>>>>> Stashed changes
